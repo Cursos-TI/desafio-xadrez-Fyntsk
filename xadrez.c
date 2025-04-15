a08dc61781ab390b99b8d33887f02818f44b4d7e
Jean Casemiro Marçal <jean.caasemiro@gmail.com>
2025-04-15 11:16:39 +0000

    Movimentacao cavalo

--- xadrez.c
+++ xadrez.c
@@ -21,5 +21,20 @@ int main() { // Torre move 5 casas para a direita
         b++;
     }   while (b < 5);
 
+    printf("\n");
+
+    for (int m = 0; m < 3; m++) { // m: número de movimentos em L
+        for (int i = 0; i < 2; i++) {
+            printf("Cavalo moveu para cima.\n");
+        }
+        for (int j = 0; j < 1; j++) {
+            printf("Cavalo moveu para direita.\n");
+        }
+        
+
     return 0;
 }
+
+
+}
+
